b19b46a82866acc24c89b4f956e594b7f9321403
Ayush Dixit <117524157+ayushdixit23@users.noreply.github.com>
2023-11-06 15:28:47 +0530

    changed

--- app/component/Header.js
+++ app/component/Header.js
@@ -15,18 +15,31 @@ function Header() {
         </div>
 
         <div
-          className="flex items-center justify-between w-48 pn:max-sm:hidden
+          className="flex items-center gap-4 justify-between pn:max-sm:hidden
       "
         >
-          <div className="hover:underline duration-75 text-lg shadow-md text-white ">
+          <Link href="/about" className="hover:underline duration-75 text-xs md:text-sm shadow-md text-white ">
             About us
-          </div>
+          </Link>
           <Link
             href={"/contact"}
-            className="hover:underline duration-75 text-lg shadow-md text-white "
+            className="hover:underline duration-75 text-xs md:text-sm shadow-md text-white "
           >
             Contant us
           </Link>
+          <Link href="/shipping" className="hover:underline duration-75 text-xs md:text-sm shadow-md text-white ">
+            Shipping
+          </Link>
+          <Link href="/cancellation" className="hover:underline duration-75 text-xs md:text-sm shadow-md text-white ">
+            Cancellation
+          </Link>
+          <Link href="/terms" className="hover:underline duration-75 text-xs md:text-sm shadow-md text-white ">
+            Terms & Conditions
+          </Link>
+          <Link href="/privacy" className="hover:underline duration-75 text-xs md:text-sm shadow-md text-white ">
+            Privacy Policy
+          </Link>
+
         </div>
         <div
           className="flex items-center justify-end w-48  sm:hidden
@@ -37,19 +50,30 @@ function Header() {
         </div>
       </div>
       <div
-        className={`absolute duration-500 sm:hidden left-0 w-full bg-white ${
-          menu ? "top-0 " : "-top-[500px]"
-        }`}
+        className={`absolute duration-500 sm:hidden left-0 w-full z-10 bg-white ${menu ? "top-0 " : "-top-[500px]"
+          }`}
       >
-        <div className="w-full justify-center items-center p-3 flex flex-col bg-black">
+        <div className="w-full justify-center items-center p-3 flex flex-col text-black">
           <div className="flex flex-col items-center w-full gap-3 pt-5">
-            <div className="text-xl font-semibold py-2 drop-shadow-[0_0px_15px_rgba(255,255,255,1)]">
-              Contact
-            </div>
-            <div className="text-xl font-semibold py-2 drop-shadow-[0_0px_15px_rgba(255,255,255,1)]">
+            <Link href="/about" className="text-xl font-semibold py-2 drop-shadow-[0_0px_15px_rgba(255,255,255,1)]">
               About Us
-            </div>
-            <div className="sm:hidden my-3">
+            </Link>
+            <Link href="/contact" className="text-xl font-semibold py-2 drop-shadow-[0_0px_15px_rgba(255,255,255,1)]">
+              Contact
+            </Link>
+            <Link href="/terms" className="text-xl font-semibold py-2 drop-shadow-[0_0px_15px_rgba(255,255,255,1)]">
+              Terms & Conditions
+            </Link>
+            <Link href="/shipping" className="text-xl font-semibold py-2 drop-shadow-[0_0px_15px_rgba(255,255,255,1)]">
+              Shipping Policy
+            </Link>
+            <Link href="/privacy" className="text-xl font-semibold py-2 drop-shadow-[0_0px_15px_rgba(255,255,255,1)]">
+              Privacy Policy
+            </Link>
+            <Link href="/cancellation" className="text-xl font-semibold py-2 drop-shadow-[0_0px_15px_rgba(255,255,255,1)]">
+              Cancellation Policy
+            </Link>
+            <div className="sm:hidden my-1">
               <div className="flex items-center flex-wrap gap-3"></div>
             </div>
             <div className="w-full px-3 pb-7">
